143412d0c26c5d37bfcad8fcc31558175f6f0e13
Rosangel593 <rosangel593@gmail.com>
2024-01-30 21:12:13 -0300

    Start endopoints

--- src/app.py
+++ src/app.py
@@ -14,16 +14,86 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://mibasededatos.db"
 db.init_app(app)
 migrate = Migrate(app, db)
 
+#GET METHOD
+
 @app.route('/', methods=['GET'])
 def User():
+    users = User.query.all()
+    users = list(map(lambda user: user.serialize_1(), users))
+    
+    return jsonify({
+        "data": users,
+        "status": 'success'
+    }),200
+    
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+#POST METHOD
 
-    return jsonify(response_body), 200
+@app.route("/create", methods=["POST"])
+def create():
+  get_from_body = request.json.get("email")
+  usuario = Ususario()
+  usuario_existente = Ususario.query.filter_by(email=get_from_body).first()
+  if usuario_existente is not None:
+    return "El usuario ya existe"
+  else:
+    usuario.nombre = request.json.get("nombre")
+    usuario.apellido = request.json.get("apellido")
+    usuario.email = request.json.get("email")
+    usuario.password = request.json.get("password")
+
+    db.session.add(usuario)
+    db.session.commit()
+
+  return f"Se creo el usuario", 201
+
+#REGISTER POST METHOD
+
+@app.route("/register", methods=["POST"])
+def register():
+  get_from_body = request.json.get("email")
+  user = User()
+  existing_user = User.query.filter_by(email=get_from_body).first()
+  existing_rut = User.query.filter_by(rut=get_from_body).first()
+  if existing_user is not None or existing_rut is not None:
+    return "User already exists"
+  else:
+    user.name = request.json.get("name")
+    user.rut = request.json.get("rut")
+    user.email = request.json.get("email")
+    user.address = request.json.get("address")
+    user.password = request.json.get("password")
+    user.phone_number = request.json.get("phone_number")
+
+    db.session.add(user)
+    db.session.commit()
+
+  return f"User created", 201
+
+#LOGIN GET METHOD
+
+@app.route("/login", methods=["GET"])
+def login():
+  get_from_body = request.json.get("email")
+  user = User()
+  existing_user = User.query.filter_by(email=get_from_body).first()
+  existing_rut = User.query.filter_by(rut=get_from_body).first()
+  if existing_user is not None or existing_rut is not None:
+    return "User already exists"
+  else:
+    user.name = request.json.get("name")
+    user.rut = request.json.get("rut")
+    user.email = request.json.get("email")
+    user.address = request.json.get("address")
+    user.password = request.json.get("password")
+    user.phone_number = request.json.get("phone_number")
+
+    db.session.add(user)
+    db.session.commit()
+
+  return f"User created", 201
+
 
 # this only runs if `$ python src/app.py` is executed
-if __name__ == '__main__':
-    #PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=5432, debug=False)
+if __name__ == "__main__":
+  app.run(host="localhost", port=5007, debug=True)
